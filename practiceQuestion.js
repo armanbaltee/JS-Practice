4086c9b9ff419260d26a95bad6a98188000d731e
armanbaltee <armanalibaltee@gmail.com>
2025-05-08 18:26:00 +0500

    feact(Problem Solve) Recursive function on array

--- practiceQuestion.js
+++ practiceQuestion.js
@@ -25,3 +25,61 @@ for(let i = 0; i<arr.length; i++){
 }
 console.log(greaterThanFifty); 
 console.log(lessThanFifty); 
+
+
+
+let arrData = [
+  {math: 90},
+  {eng: 70},
+  {phy: 80},
+  {chem: 60}
+]
+let length = 0
+function getValue(arr){
+    if(length<arr.length){
+      let data = arr[length]
+      for(let key in data){
+        console.log(`key is ${key} and value is ${data[key]}`)
+      }
+    }else{
+      return
+    }
+    length++
+    getValue(arr)
+}
+getValue(arrData);
+
+
+let myArr = ["Hello", "World", "Parallel"];
+
+let myString = ""
+let length1 = 0;
+function convertToString(arr){
+  if(length1<arr.length){
+    myString += arr[length1]
+  }else{
+    return
+  }
+  length1++
+  convertToString(arr)
+}
+convertToString(myArr)
+console.log(myString)
+
+
+let data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let arrLength = 0;
+let evenNumber = []
+function getEvenNumber(arr){
+  if(arrLength<arr.length){
+    if(arr[arrLength]%2===0){
+      evenNumber.push(arr[arrLength])
+    }
+    }else{
+      return
+    }
+    arrLength++
+  getEvenNumber(arr)
+}
+getEvenNumber(data);
+console.log(evenNumber)
